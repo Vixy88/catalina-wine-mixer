7ca9ee9d32b8a2934ed071789842939da7d6dd4e
Michael Jakobsen <michael@rpair.co.uk>
2022-01-08 18:15:31 +0000

    changed the grid creation function to only create 99 cells instead of 100

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -11,7 +11,7 @@ const cells = [];
 
 // CREATE GRID
 function createGrid() {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 99; i++) {
     const gridDiv = document.createElement('div');
     gridDiv.innerText = [i];
     grid.append(gridDiv);
