91473fba023b1ff9fcb9a68f59096a3f8f3345f2
Michael Jakobsen <michael@rpair.co.uk>
2022-01-08 18:11:00 +0000

    Added more gamelogic including the countdown clock and player score fields

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -1,8 +1,5 @@
 console.log('TEST: JavaScript file is working');
 
-//  VARIABLES
-
-
 //GRID
 const gridWrapper = document.querySelector('.grid-wrapper');
 const grid = document.querySelector('.grid');
@@ -23,10 +20,6 @@ function createGrid() {
 
 createGrid();
 
-
-// CHARACTER ELEMENTS
-
-
 // PLAYER CONTROLLER
 const playerOne = document.querySelector('.character');
 let playerX = 0;
@@ -34,21 +27,23 @@ let playerY = 0;
 
 const handleKeydown = (event) => {
   // LEFT AND RIGHT
-  if (event.code === 'ArrowRight' && playerX <= 860) {
-    // move player right
+  if (event.code === 'ArrowRight' && playerX <= 840) {
+    // move player right but stop if reach the border
     playerOne.style.left = playerX + 'px';
-    playerX += 15;
+    playerX += 20;
   }
   if (event.code === 'ArrowLeft' && playerX >= 5) {
-    // move player left
+    // move player left but stop if reach the border
     playerOne.style.left = playerX + 'px';
     playerX -= 20;
   }
-  if (event.code === 'ArrowDown' && playerY <= 850) {
+  if (event.code === 'ArrowDown' && playerY <= 410) {
+    // move player down but stop if reach the border
     playerOne.style.top = playerY + 'px';
     playerY += 20;
   }
   if (event.code === 'ArrowUp' && playerY >= 0) {
+    // move player up but stop if reach the border
     playerOne.style.top = playerY + 'px';
     playerY -= 20;
   }
@@ -56,17 +51,35 @@ const handleKeydown = (event) => {
 
 document.addEventListener('keydown', handleKeydown);
 
+// COUNTDOWN TIMER LOGIC
+
+const countDownClock = document.querySelector('#countDownTimer');
+let startTime = 5;
+
+function countDownTimer() {
+  setInterval(function () {
+    if (startTime === 0) {
+      clearInterval(setInterval);
+      countDownClock.textContent = 'OH NO!!! You ruined the Catalina F***** Winemixer';
+    } else {
+      countDownClock.textContent = startTime;
+      startTime -= 1;
+    }
+
+  }, 1000);
+}
+
 // START GAME LOGIC
 
 const startButton = document.querySelector('.startBtn'); // GRAB BUTTON and store in variable
+const gameScreenDisplay = document.querySelector('.display-game-section');
+const landingPageScreen = document.querySelector('.landing-page-section');
 
 const startGame = () => {
-  gridWrapper.classList.remove('hide'); // removes the class that hides the game screen on DOM load
+  gameScreenDisplay.classList.remove('hide'); // removes the class that hides the game screen on DOM load
+  landingPageScreen.classList.add('hide'); // adds the class that hides the landing page screen
+  countDownTimer();
+  console.log('TEST: The Game successfully started');
 };
 
 startButton.addEventListener('click', startGame);
-
-
-
-
-
