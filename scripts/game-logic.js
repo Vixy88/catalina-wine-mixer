2c0ee7437d20f0ad07426b586da19d740774f265
Michael Jakobsen <michael@rpair.co.uk>
2022-01-20 21:34:10 +0000

    fixed the restart button bug that caused the grid to be duplicated, added TRY AGAIN buttons for end of game screens

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -9,13 +9,16 @@ const gameScreenDisplay = document.querySelector('.display-game-section');
 const landingPageScreen = document.querySelector('.landing-page-section');
 const grid = document.querySelector('.grid');
 const gridWrapper = document.querySelector('.grid-wrapper');
-const numberOfCells = 176;
+const numberOfCells = 108;
 const cells = [];
-const rowLength = 22;
+const rowLength = 18;
 const champagneBottles = [];
+
+let currentPlayer = 'characterDale';
 
 // CREATE GRID
 function createGrid() {
+  grid.innerHTML = ''; // HTML inside of grid is set as blank
   for (let i = 0; i < numberOfCells; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -25,19 +28,14 @@ function createGrid() {
       champagneBottles.push(champagneBottle);
     }
     grid.append(cell);
-    cells.push(cell);
+    cells[i] = cell;
   }
 }
 
 // Function that adds the class containing the image of the Dale Character to the first cell on the grid
-function addCharacterDale() {
+function addCharacter() {
   let characterStartingPosition = document.querySelector('.cell');
-  characterStartingPosition.classList.add('characterDale');
-}
-
-function addCharacterBrennan() {
-  let characterStartingPosition = document.querySelector('.cell');
-  characterStartingPosition.classList.add('characterBrennan');
+  characterStartingPosition.classList.add(currentPlayer);
 }
 
 // PLAYER CONTROLLER
@@ -45,11 +43,11 @@ let playerScore = 0;
 let playerCurrentPosition = 0;
 
 const handleKeydown = (event) => {
-  const playerDiv = document.querySelector('.cell.characterDale');
+  const playerDiv = document.querySelector(`.${currentPlayer}`);
   if (event.code === 'ArrowRight' && playerCurrentPosition < 176) {
-    cells[playerCurrentPosition].classList.remove('characterDale');
+    cells[playerCurrentPosition].classList.remove(currentPlayer);
     playerCurrentPosition += 1;
-    cells[playerCurrentPosition].classList.add('characterDale');
+    cells[playerCurrentPosition].classList.add(currentPlayer);
     console.log(`Player Index position: ${playerCurrentPosition}`);
     // CHAMPAGNE COLLECTION LOGIC
     if (playerDiv.classList.contains('champagneBottle')) {
@@ -69,9 +67,9 @@ const handleKeydown = (event) => {
   }
   if (event.code === 'ArrowLeft' && playerCurrentPosition > 0) {
     // move player left but stop if reach the border
-    cells[playerCurrentPosition].classList.remove('characterDale');
+    cells[playerCurrentPosition].classList.remove(currentPlayer);
     playerCurrentPosition -= 1;
-    cells[playerCurrentPosition].classList.add('characterDale');
+    cells[playerCurrentPosition].classList.add(currentPlayer);
     console.log(`Player Index position: ${playerCurrentPosition}`);
     // CHAMPAGNE COLLECTION LOGIC
     if (playerDiv.classList.contains('champagneBottle')) {
@@ -91,9 +89,9 @@ const handleKeydown = (event) => {
   }
   if (event.code === 'ArrowDown' && playerCurrentPosition < 154) {
     // move player down but stop if reach the border
-    cells[playerCurrentPosition].classList.remove('characterDale');
+    cells[playerCurrentPosition].classList.remove(currentPlayer);
     playerCurrentPosition += rowLength;
-    cells[playerCurrentPosition].classList.add('characterDale');
+    cells[playerCurrentPosition].classList.add(currentPlayer);
     console.log(`Player Index position: ${playerCurrentPosition}`);
     // CHAMPAGNE COLLECTION LOGIC
     if (playerDiv.classList.contains('champagneBottle')) {
@@ -113,9 +111,9 @@ const handleKeydown = (event) => {
   }
   if (event.code === 'ArrowUp' && playerCurrentPosition > 22) {
     // move player up but stop if reach the border
-    cells[playerCurrentPosition].classList.remove('characterDale');
+    cells[playerCurrentPosition].classList.remove(currentPlayer);
     playerCurrentPosition -= rowLength;
-    cells[playerCurrentPosition].classList.add('characterDale');
+    cells[playerCurrentPosition].classList.add(currentPlayer);
     console.log(`Player Index position: ${playerCurrentPosition}`);
     // CHAMPAGNE COLLECTION LOGIC
     if (playerDiv.classList.contains('champagneBottle')) {
@@ -160,46 +158,42 @@ function countDownTimer() {
 
 // START GAME LOGIC
 
-const startGameDale = () => {
+function startGame() {
   gameScreenDisplay.classList.remove('hide'); // removes the class that hides the game screen on DOM load
   landingPageScreen.classList.add('hide'); // adds the class that hides the landing page screen
   createGrid();
-  addCharacterDale();
+  addCharacter();
   countDownTimer();
+}
+
+const startGameDale = () => {
+  currentPlayer = 'characterDale';
+  startGame();
   console.log('TEST: The Game successfully started, playing with Dale');
 };
 
 const startGameBrennan = () => {
-  gameScreenDisplay.classList.remove('hide'); // removes the class that hides the game screen on DOM load
-  landingPageScreen.classList.add('hide'); // adds the class that hides the landing page screen
-  createGrid();
-  addCharacterBrennan();
-  countDownTimer();
+  currentPlayer = 'characterBrennan';
+  startGame();
   console.log('TEST: The Game successfully started, playing with Brennan');
 };
 
 // restarts the game if the player wants to - this will restart the whole game when clicked
 function restartGameBrennan() {
   if (confirm('Are you sure you want to restart the game? - you will lose all your progress')) {
-    // window.location.reload(false);
-    resetGlobalVariables();
+    startTime = 25; // reset start time to 25 seconds
+    playerScore = 0; // reset player score to 0
+    Number(document.getElementById('playerScore').innerHTML = playerScore);
+    // Create new grid
+    createGrid();
+    // Reset Players Position to start position
+    playerCurrentPosition = 0;
+    cells[playerCurrentPosition].classList.add(currentPlayer);
   }
 }
 
-function resetGlobalVariables() {
-  startTime = 25; // reset start time to 25 seconds
-  playerScore = 0; // reset player score to 0
-  Number(document.getElementById('playerScore').innerHTML = playerScore);
-  // Reset Players Position to start position
-  cells[playerCurrentPosition].classList.remove('characterDale');
-  playerCurrentPosition = 0;
-  cells[playerCurrentPosition].classList.add('characterDale');
-  // Remove current Grid
-  gridWrapper.remove('cell');
-  // Create new grid
-  const grid = document.createElement('div');
-  grid.classList.add('grid');
-  gameScreenDisplay.append(grid);
+function launchNewGame() {
+  window.location.reload(false);
 }
 
 // Event listeners
